d9f22e8be3ab67109260442e2b57e29ba896444d
josue rojas <josuerojas.rojas@gmail.com>
2018-06-13 11:10:52 -0400

    added slideshow logic

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,7 @@
-console.log('hello');
+$( document ).ready(function(){
+  // handle slideshow movements onclick dots
+  $('.slideshow-wrapper .dots-wrapper .dot').click(function(){
+    const index = $(this).index();
+    $('.slideshow-wrapper .slideshow-view').css('transform', `translateX(${index*-100}vw)`);
+  })
+});
