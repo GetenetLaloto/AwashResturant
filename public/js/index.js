17c0f7ecb637a03a1543bf1c1a6e23d4a390528f
josue rojas <josuerojas.rojas@gmail.com>
2018-06-14 14:57:57 -0400

    fixed bug where would stop scrolling caused by transform!=scroll; fixed bug where dot clicked would not transform correctly; instead using animate and scroll from jquery

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,8 @@
 $( document ).ready(function(){
-  // TODO: one bug where touch scroll would create offset that is not match when translateX
-  // TODO: one bug where touch scroll will stop scrolling to the left
-  // handle slideshow movements onclick dots
   $('.slideshow .dots-wrapper .dot').click(function(){
     const index = $(this).index();
-    $('.slideshow .slideshow-wrapper .slideshow-view').css('transform', `translateX(${index*-100}vw)`);
+    const viewportWidth = $(window).width();
+    $('.slideshow .slideshow-wrapper').animate({scrollLeft:(index*viewportWidth)}, 600);
   });
 
 });
