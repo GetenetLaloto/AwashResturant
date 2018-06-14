71788274860fadc2d62f3bb57b3adf0504d1edd2
josue rojas <josuerojas.rojas@gmail.com>
2018-06-14 14:23:31 -0400

    addded touch scroll

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,10 @@
 $( document ).ready(function(){
+  // TODO: one bug where touch scroll would create offset that is not match when translateX
+  // TODO: one bug where touch scroll will stop scrolling to the left
   // handle slideshow movements onclick dots
-  $('.slideshow-wrapper .dots-wrapper .dot').click(function(){
+  $('.slideshow .dots-wrapper .dot').click(function(){
     const index = $(this).index();
-    $('.slideshow-wrapper .slideshow-view').css('transform', `translateX(${index*-100}vw)`);
+    $('.slideshow .slideshow-wrapper .slideshow-view').css('transform', `translateX(${index*-100}vw)`);
   });
 
 });
